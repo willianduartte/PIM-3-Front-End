0cd987303af06a23c4d7862c776cde4615e1b334
Willianduarttee <william.duarte@amktechsistemas.com.br>
2026-05-15 22:10:59 -0300

    first commit

---
+++ assets/js/pizzas.js
@@ -0,0 +1,8 @@
+let pizzaJson = [
+    {id:1, name:'Calabresa Especial', img:'assets/images/pizza.png', price:[20.00, 35.00, 45.90], sizes:['320g', '530g', '860g'], description:'Calabresa premium defumada, cebola roxa e um toque de orégano fresco sobre massa artesanal.'},
+    {id:2, name:'Margherita Premium', img:'assets/images/pizza2.png', price:[25.00, 40.00, 49.90], sizes:['320g', '530g', '860g'], description:'Muçarela de búfala, manjericão orgânico e fatias de tomate italiano selecionados.'},
+    {id:3, name:'Frango com Catupiry', img:'assets/images/pizza3.png', price:[22.00, 38.00, 52.90], sizes:['320g', '530g', '860g'], description:'Frango desfiado temperado com ervas finas e o legítimo Catupiry original.'},
+    {id:4, name:'Ouriço Quatro Queijos', img:'assets/images/pizza4.png', price:[28.00, 45.00, 55.00], sizes:['320g', '530g', '860g'], description:'A combinação perfeita de Muçarela, Provolone, Parmesão e Gorgonzola.'},
+    {id:5, name:'Portuguesa LaVai', img:'assets/images/pizza5.png', price:[24.00, 39.00, 50.00], sizes:['320g', '530g', '860g'], description:'Presunto, ovos, cebola, ervilha e azeitonas pretas sobre molho de tomate pelati.'},
+    {id:6, name:'Chocolate com Avelã', img:'assets/images/pizza7.png', price:[30.00, 45.00, 58.00], sizes:['320g', '530g', '860g'], description:'Chocolate meio amargo derretido com creme de avelã e pedaços de castanha.'}
+];
